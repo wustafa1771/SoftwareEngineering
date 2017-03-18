2d2de0cbcb922c1ed401c7d26803fedc65a62058
ecimen96 <ecimen96@hotmail.com>
2017-03-18 13:11:27 +0300

    Created Java Project With Eclipse

---
+++ RushHour/src/rushhour/Main.java
@@ -0,0 +1,15 @@
+package rushhour;
+
+/**
+ * Main class for the game Rush Hour
+ * @author egemen
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
