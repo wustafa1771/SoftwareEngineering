24b4bbc69cce87575b980e53fea62461cfa2cca2
ecimen96 <ecimen96@hotmail.com>
2017-03-18 13:35:02 +0300

    JFrame & Runnable

--- RushHour/src/rushhour/Main.java
+++ RushHour/src/rushhour/Main.java
@@ -1,15 +1,28 @@
 package rushhour;
 
+import javax.swing.JFrame;
+
 /**
  * Main class for the game Rush Hour
  * @author egemen
  *
  */
-public class Main {
+public class Main extends JFrame implements Runnable
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
 
+		
+		
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
